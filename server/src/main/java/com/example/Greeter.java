34cd8d4c1ebd8a2b19dc0b1e8e68fcdc41f21494
Herve <dockerlite@gmail.com>
2018-11-23 17:59:07 +0100

    change

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,14 +13,7 @@ public class Greeter {
    * This is a constructor.
    */
   public Greeter() {
-    try {
-      Map map = System.getProperties();
-      for(;;) {
-        map.put(new MemLeak("&amp;quot;key&amp;quot"), "test");
-      }
-    } catch(Exception e) {
-      e.printStackTrace();
-    }
+
   }
 
 
