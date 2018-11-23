9d47dc731bc10337eb456145523dc8f1f9abe2d4
Herve <dockerlite@gmail.com>
2018-11-23 16:33:03 +0100

    change

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,6 +1,10 @@
 package com.example;
 
-/**
+
+import java.util.Map;
+
+
+  /**
  * This is a class.
  */
 public class Greeter {
@@ -9,8 +13,17 @@ public class Greeter {
    * This is a constructor.
    */
   public Greeter() {
-
+    try {
+      Map map = System.getProperties();
+      for(;;) {
+        map.put(new MemLeak("&amp;quot;key&amp;quot"), "test");
+      }
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
   }
+
+
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
