3fb5408925bd07a8349922874f08b1fbd953d7fa
Herve <dockerlite@gmail.com>
2018-11-24 15:18:52 +0100

    more fixes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -21,8 +21,7 @@ public class Greeter {
      * @param someone
      * @return
      */
-    
-  public final String greet( final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
