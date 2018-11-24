04118ce435592cf1f942b1121c6e61fbfd69a4eb
Herve <dockerlite@gmail.com>
2018-11-24 15:14:39 +0100

    fix checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,10 +16,13 @@ public class Greeter {
 
   }
 
-
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+    /**
+     *
+     * @param someone
+     * @return
+     */
+    
+  public final String greet( final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
